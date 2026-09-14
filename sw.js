47b291454d1bb5fd8c6037875e347c41c1d78d7f
dsherifsaleh-ship-it <d.sherifsaleh@gmail.com>
2026-09-14 03:16:52 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'borg-makka-v17';
-const ASSETS = ['./', './index.html', './manifest.json', './icon.svg'];
+const CACHE = 'borg-makka-v19';
+const ASSETS = ['./','./index.html','./manifest.json','./icon.svg'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).catch(()=>{}));
   self.skipWaiting();
@@ -7,18 +7,16 @@ self.addEventListener('install', event => {
 self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
-      .then(() => self.clients.claim())
+      .then(()=>self.clients.claim())
   );
 });
 self.addEventListener('fetch', event => {
   if (event.request.method !== 'GET') return;
   event.respondWith(
     fetch(event.request).then(response => {
-      if (response && response.ok) {
-        const copy=response.clone();
-        caches.open(CACHE).then(c=>c.put(event.request,copy)).catch(()=>{});
-      }
+      const copy=response.clone();
+      caches.open(CACHE).then(c=>c.put(event.request,copy)).catch(()=>{});
       return response;
-    }).catch(() => caches.match(event.request).then(r=>r || caches.match('./index.html')))
+    }).catch(()=>caches.match(event.request).then(r=>r || caches.match('./index.html')))
   );
 });
